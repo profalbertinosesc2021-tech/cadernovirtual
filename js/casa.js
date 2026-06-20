311053f3b49a612a286f01e3fd07ae396808ddb3
profalbertinosesc2021-tech <profalbertinosesc2021@gmail.com>
2026-06-20 15:47:26 -0300

    Create casa.js

---
+++ js/casa.js
@@ -0,0 +1,28 @@
+
+const paginas = {
+// ==== Violão Popular ====
+    "1234": "paginas/violaopopular/aula1.html",
+    "1234": "paginas/violaopopular/aula2.html",
+
+  // ===========
+    "teoria789": "paginas/aula3.html",
+    "harmonia321": "paginas/aula4.html"
+
+    // futuramente:
+    // "senha500": "paginas/aula500.html"
+};
+
+
+function entrar(){
+
+    let senha = document.getElementById("senha").value;
+
+    if (paginas[senha]){
+        window.location.href = paginas[senha];
+    }
+    else{
+        document.getElementById("mensagem").innerHTML =
+        "Senha inválida!";
+    }
+
+}
